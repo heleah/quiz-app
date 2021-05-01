6d01261e3882eff0a15d011279f6077213b44ab5
Helena Heil <helenahcodes@gmail.com>
2021-05-01 19:07:17 +0200

    rebuild quizcard answer mechanics

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
 import showAnswer from './lib/answer-button';
 import createCard, { quizCards } from './lib/quizcards';
+import { bookmarkedCards } from './lib/bookmarks';
 
-createCard(quizCards);
-showAnswer();
+const pageTitle = document.querySelector('h1');
+
+switch (pageTitle.innerText) {
+  case 'Quizney':
+    createCard(quizCards);
+    showAnswer();
+    break;
+  case 'Bookmarks':
+    createCard(bookmarkedCards);
+    showAnswer();
+    break;
+}
